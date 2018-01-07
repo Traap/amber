8a5f6472b2c16da4d577b4baefdb8e39c93bee39
Traap <gary.a.howard@mac.com>
2018-01-07 15:14:03 -0600

    Closes #13 - Remove obsolete files.

--- test-runner/src/TestRunner/Program.hs
+++
@@ -1,8 +0,0 @@
-module TestRunner.Program (Program(..)) where
-
--- Type: Program
-data Program = PROGRAM
-                 {p :: String     -- Command to run.
-                 ,a :: [String]   -- Arguments to pass to command.
-                 } deriving (Show)
-
--- test-runner/src/TestRunner/Protocol.hs
+++
@@ -1,9 +0,0 @@
-module TestRunner.Protocol (Protocol(..)) where
-
--- Type: Protocol
-
-data Protocol = PROTOCOL
-  {pn :: IO ()            -- Protocol name.
-  ,msg :: String          -- Message text.
-  }
-
