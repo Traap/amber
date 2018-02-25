ccf6adfab7b352c1eb10d6090330250fba94013f
Traap <gary.a.howard@mac.com>
2018-02-25 13:12:18 -0600

    Closes #27 - environment texfile annotated with LaTeX syntax.

--- lib/amber/latex-environment.rb
+++ lib/amber/latex-environment.rb
@@ -12,14 +12,16 @@ module Amber
 
     def echo_to_sysout
       @handle = TestEvidence::open_environment_log_file(@options)
-      @handle.write "\\subsection{System Environment}\n"
+      write_subsection
       @handle.write "\\begin{description}[align=right,leftmargin=*,labelindent=3cm]\n"
 
       @decoratee.environment.each do |e|
-        if e == "PATH" then
+        if e.downcase.include?("path") then
           echo_e_to_sysout(e)
         else
-          @handle.write "\\item[#{e}:] #{ENV[e]}\n"
+          i = replace_underscore_with_hyphen(e)
+          v = replace_backslash_with_forwardslash("#{ENV[e]}")
+          @handle.write "\\item[#{i}:] #{v}\n"
         end
       end
 
@@ -28,12 +30,31 @@ module Amber
     end
 
     def echo_e_to_sysout(e)
-      @handle.write "\\item[#{e}:] "
+      i = replace_underscore_with_hyphen(e) 
+      @handle.write "\\item[#{i}:] See below.\n"
+      @handle.write "\\begin{enumerate}\n"
       f = ENV[e].split(':')
       f.each do |g|
-        @handle.write "#{g} "
+        v = replace_backslash_with_forwardslash(g)
+        @handle.write "\\item #{v}\n"
       end
-      @handle.write "\n"
+      @handle.write "\\end{enumerate}\n"
+    end
+
+    private
+    def write_subsection
+      @handle.write "\\subsection{System Environment}\n"
+      @handle.write "The hyphen character replaced the underscore character,\n"
+      @handle.write "and the forward slash character replaced the backslash\n"
+      @handle.write "character throughout this section.\n"
+    end
+
+    def replace_underscore_with_hyphen(text)
+      text.tr("_", "-")
+    end
+
+    def replace_backslash_with_forwardslash(text)
+      text.tr("\\", "/")
     end
 
   end #LaTeX_Environment
