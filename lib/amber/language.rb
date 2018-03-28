954d4695f7e47a4829dde5310a525026d6fe78d7
Traap <gary.a.howard@mac.com>
2018-03-28 09:32:28 -0500

    Update #62 - ge => german removed.

--- lib/amber/language.rb
+++ lib/amber/language.rb
@@ -1,13 +1,20 @@
 module Amber
   module Language
 
-    Names = %w[zz da de en es fr-ca fr-eu ge it ne no sv]
+    Names = %w[zz da de en es fr-ca fr-eu it ne no sv]
 
-    Code  = {"zz" => "n/a", "da" => "Dansk", "de" => "Deutsch", "en"=> "English",
-            "es" => "Espanol", "fr-ca" => "CA French - Canadian",
-            "fr-eu" => "EU French - European", "ge" => "German",
-            "it" => "Italiano", "ne" => "Nederlands", "no" => "Norsk",
-            "sv" => "Svenska"}
+    Code = {"zz" => "n/a",
+            "da" => "Dansk",
+            "de" => "Deutsch",
+            "en"=> "English",
+            "es" => "Espanol",
+            "fr-ca" => "CA French - Canadian",
+            "fr-eu" => "EU French - European",
+            "it" => "Italiano",
+            "ne" => "Nederlands",
+            "no" => "Norsk",
+            "sv" => "Svenska"
+           }
 
     Default = "zz"
 
