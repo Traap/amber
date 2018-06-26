74a7c55042f59ab560e61179783c9150548ef99d
Traap <gary.a.howard@mac.com>
2018-06-26 05:41:26 -0500

    Closed #73 - Czech, Polish, and Romanian have been added.

--- lib/amber/language.rb
+++ lib/amber/language.rb
@@ -1,9 +1,10 @@
 # Languages that Amber support.
 module Amber
   module Language
-    NAMES = %w[zz da de en es fr-ca fr-eu it ne no sv].freeze
+    NAMES = %w[zz cs da de en es fr-ca fr-eu it ne no pl ro sv].freeze
 
     CODE = { 'zz' => 'n/a',
+             'cs' => 'Czech',
              'da' => 'Dansk',
              'de' => 'Deutsch',
              'en' => 'English',
@@ -13,6 +14,8 @@ module Amber
              'it' => 'Italiano',
              'ne' => 'Nederlands',
              'no' => 'Norsk',
+             'pl' => 'Polish',
+             'ro' => 'Romanian',
              'sv' => 'Svenska' }.freeze
 
     DEFAULT = 'zz'.freeze
