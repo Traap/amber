7662717efd339ee464975dcd685c85739fbf50f7
Traap <gary.a.howard@mac.com>
2018-12-18 17:25:20 -0600

    Rspec for browser substitutions

--- spec/utility/substitute_spec.rb
+++ spec/utility/substitute_spec.rb
@@ -5,6 +5,10 @@ require 'amber'
 # ------------------------------------------------------------------------------
 describe 'YAML file Substitution' do
 
+  before(:all) do
+    @options = Amber::Options.new
+  end
+
   # ----------------------------------------------------------------------------
   describe 'File name' do
 
@@ -44,7 +48,51 @@ describe 'YAML file Substitution' do
     end
 
   end
+
   # ----------------------------------------------------------------------------
+  describe 'Browser' do
+
+    it "can substitute ${BROWSER} to #{Amber::Browser::DEFAULT}" do
+      expect(Amber::Substitute
+        .browser(@options, "${BROWSER}")).to eql(Amber::Browser::DEFAULT)
+    end
+
+    it "can substitute ${browser} to #{Amber::Browser::DEFAULT}" do
+      expect(Amber::Substitute
+        .browser(@options, "${browser}")).to eql(Amber::Browser::DEFAULT)
+    end
+
+    it 'can substitute ${BROWSER} to Chrome' do
+      @options.browser = 'Chrome' 
+      expect(Amber::Substitute
+        .browser(@options, '${BROWSER}')).to eql('Chrome')
+    end
+
+    it 'can substitute ${browser} to Edge' do
+      @options.browser = 'Edge' 
+      expect(Amber::Substitute
+        .browser(@options, '${browser}')).to eql('Edge')
+    end
+
+    it 'can substitute ${BROWSER} to Firefox' do
+      @options.browser = 'Firefox' 
+      expect(Amber::Substitute
+        .browser(@options, '${BROWSER}')).to eql('Firefox')
+    end
+
+    it 'can substitute ${browser} to IE' do
+      @options.browser = 'IE'
+      expect(Amber::Substitute
+        .browser(@options, '${browser}')).to eql('IE')
+    end
+
+    it 'can substitute ${BROWSER} to Opera' do
+      @options.browser = 'Opera' 
+      expect(Amber::Substitute
+        .browser(@options, '${BROWSER}')).to eql('Opera')
+    end
+
+  end
 
 end
 
