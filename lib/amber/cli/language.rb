bad08ec69d7a979c1d427c225f46b303a44bfa32
nbockrath <nathan.bockrath@fresenius-kabi.com>
2019-06-07 14:56:31 -0500

    Language Updates. AUTO-2097

--- lib/amber/cli/language.rb
+++ lib/amber/cli/language.rb
@@ -3,21 +3,25 @@
 # Languages that are supported.  The default is zz.
 module Amber
   module Language
-    NAMES = %w[zz cs da de en es fr-ca fr-eu it ne no pl ro sv].freeze
+    NAMES = %w[zz cs da de en en-US es fr-ca fr-eu hu it ne no pl pt ro sk sv].freeze
 
     CODE = { 'zz' => 'n/a',
              'cs' => 'Czech',
              'da' => 'Dansk',
              'de' => 'Deutsch',
              'en' => 'English',
+             'en-US' => 'US English',
              'es' => 'Espanol',
              'fr-ca' => 'CA French - Canadian',
              'fr-eu' => 'EU French - European',
+             'hu' => 'Hungarian',
              'it' => 'Italiano',
              'ne' => 'Nederlands',
              'no' => 'Norsk',
              'pl' => 'Polish',
+             'pt' => 'Portuguese',
              'ro' => 'Romanian',
+             'sk' => 'Slovak',
              'sv' => 'Svenska' }.freeze
 
     DEFAULT = 'zz'.freeze
