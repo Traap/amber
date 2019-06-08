4d56e8785f6af02572e262530f3337b460f2e3bd
nbockrath <nathan.bockrath@fresenius-kabi.com>
2019-06-07 19:10:51 -0500

    Update specs for language CLO, substitution. AUTO-2097

--- lib/amber/cli/language.rb
+++ lib/amber/cli/language.rb
@@ -3,20 +3,21 @@
 # Languages that are supported.  The default is zz.
 module Amber
   module Language
-    NAMES = %w[zz cs da de en en-US es fr-ca fr-eu hu it ne no pl pt ro sk sv].freeze
+    NAMES = %w[zz cs da de en en-US es fr fr-ca fr-eu hu it ne no pl pt ro sk sv].freeze
 
     CODE = { 'zz' => 'n/a',
              'cs' => 'Czech',
              'da' => 'Dansk',
              'de' => 'Deutsch',
              'en' => 'English',
-             'en-US' => 'US English',
+             'en-US' => 'US English',  # variant found on the VMM
              'es' => 'Espanol',
+             'fr' => 'French',  # variant found on the VMM
              'fr-ca' => 'CA French - Canadian',
              'fr-eu' => 'EU French - European',
              'hu' => 'Hungarian',
              'it' => 'Italiano',
-             'ne' => 'Nederlands',
+             'nl' => 'Nederlands',  # variant found on the VMM. Code 'ne' found in IS-639-1 is Nepali.
              'no' => 'Norsk',
              'pl' => 'Polish',
              'pt' => 'Portuguese',
